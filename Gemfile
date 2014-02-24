67380d22e00902960d51b2050677e16a46b006c5
Álvaro Fernando <alvarofernandoms@hotmail.com>
2014-02-24 20:29:32 -0300

    Testing with Guard and Spork

--- Gemfile
+++ Gemfile
@@ -7,16 +7,27 @@ gem 'rails', '4.0.1'
 group :development, :test do
   gem 'sqlite3', '1.3.8'
   gem 'rspec-rails', '2.13.1'
+  gem 'guard-rspec', '1.2.1'
+  gem 'guard-spork', '1.2.0'
+  gem 'childprocess', '0.5.1'
+  gem 'spork', '0.9.2'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '4.0.1'
+  gem 'coffee-rails', '4.0.1'
+  gem 'uglifier', '2.1.1'
 end
 
 group :test do
-  gem 'selenium-webdriver', '2.35.1'
-  gem 'capybara', '2.1.0'
+  gem 'selenium-webdriver', '2.40.0'
+  gem 'capybara', '1.1.2'
+  gem 'rb-inotify', '0.8.8'
+  gem 'libnotify', '0.5.9'
 end
 
-gem 'sass-rails', '4.0.1'
-gem 'uglifier', '2.1.1'
-gem 'coffee-rails', '4.0.1'
 gem 'jquery-rails', '3.0.4'
 gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '1.0.2'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,57 +2,57 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-	
-	describe "Home page" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
-		it "Should have the content 'Sample App' " do
-			visit '/static_pages/home'
-			expect(page).to have_content('Sample App')
-		end
+  describe "Home page" do
 
-		it "should have the title 'Home'" do
-			visit '/static_pages/home'
-			expect(page).to have_title("#{base_title} | Home")
-		end
-	end
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
 
-	describe "Help page" do
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "#{base_title} | Home")
+    end
+  end
 
-		it "should have the content 'Help' " do
-			visit '/static_pages/help'
-			expect(page).to have_content('Help')
-		end
+  describe "Help page" do
 
-		it "should have the title 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_title("#{base_title} | Help")
-		end
-	end
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
 
-	describe "About page" do
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "#{base_title} | Help")
+    end
+  end
 
-		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
-			expect(page).to have_content('About Us')
-		end
+  describe "About page" do
 
-		it "shoud have the title 'About Us'" do
-			visit '/static_pages/about'
-			expect(page).to have_title("#{base_title} | About Us")
-		end
-	end
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
 
-	describe "Contact" do
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "#{base_title} | About Us")
+    end
+  end
 
-		it "should have the content 'Contact'" do
-			visit '/static_pages/contact'
-			expect(page).to have_content('Contact')
-		end
+  describe "Contact page" do
 
-		it "shoud have the title 'Contact'" do
-			visit '/static_pages/contact'
-			expect(page).to have_title("#{base_title} | Contact")
-		end
-	end
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
+  end
 end
