a3cdc0bd948a09f1e8e0be82f78ad2848f7957c7
alvarofernandoms <alvarofernandoms@vm-0.alvarofernandoms.koding.kd.io>
2014-03-04 02:54:28 +0000

    Testing Koding.com

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,9 @@ Spork.prefork do
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
+  
+  # ESTE É UM TESTE DO KODING.COM
+  # eHuehUHuehuehUHUe BR? BR? BR! Hu3!
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
